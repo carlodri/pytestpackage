0920a7ad04b29d8ec89660ae6aac72d815c7c12b
Carlo Dri <carlodri@users.noreply.github.com>
2015-04-16 00:18:50 +0200

    Update pytestpackage.py

--- pytestpackage/pytestpackage.py
+++ pytestpackage/pytestpackage.py
@@ -1,3 +1,8 @@
+'''This is a very important module
+
+This is a very important module that is needed to do seomthing really cool things and blablabla
+'''
+
 import numpy as np
 
 def testpr(arg1):
@@ -7,3 +12,14 @@ def testpr(arg1):
 		arg1 (string): the string to be printed
 	'''
 	print(arg1)
+	
+def newfunc(arg1, arg2):
+	'''A very useful second function!
+	
+	This is a very lengthy text to show how blablab to show how blablabto show ho
+
+	Args:
+		arg1 (string): the string to be printed
+		arg2 (int): a useless argument
+	'''
+	print('Hiiii!')
